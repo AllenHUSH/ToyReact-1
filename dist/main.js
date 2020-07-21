a94fe9c49f54f55f381bc75ab8be5752698d4157
义晋 胡 <jingdiqkl123@foxmail.com>
2020-07-21 22:18:23 +0800

    tic tac toe

--- dist/main.js
+++ dist/main.js
@@ -97,13 +97,13 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Component", function() { return Component; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ToyReact", function() { return ToyReact; });
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
 function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -122,17 +122,39 @@ var ElementWrapper = /*#__PURE__*/function () {
   _createClass(ElementWrapper, [{
     key: "setAttribute",
     value: function setAttribute(name, value) {
+      if (name.match(/^on([\s\S]+)$/)) {
+        var eventName = RegExp.$1.replace(/^[\s\S]/, function (s) {
+          return s.toLowerCase();
+        });
+        this.root.addEventListener(eventName, value);
+      }
+
+      if (name === 'className') {
+        name = 'class';
+      }
+
       this.root.setAttribute(name, value);
     }
   }, {
     key: "appendChild",
     value: function appendChild(vchild) {
-      vchild.mountTo(this.root);
+      var range = document.createRange();
+
+      if (this.root.children.length) {
+        range.setStartAfter(this.root.lastChild);
+        range.setEndAfter(this.root.lastChild);
+      } else {
+        range.setStart(this.root, 0);
+        range.setEnd(this.root, 0);
+      }
+
+      vchild.mountTo(range);
     }
   }, {
     key: "mountTo",
-    value: function mountTo(parent) {
-      parent.appendChild(this.root);
+    value: function mountTo(range) {
+      range.deleteContents();
+      range.insertNode(this.root);
     }
   }]);
 
@@ -149,8 +171,9 @@ var TextWrapper = /*#__PURE__*/function () {
 
   _createClass(TextWrapper, [{
     key: "mountTo",
-    value: function mountTo(parent) {
-      parent.appendChild(this.root);
+    value: function mountTo(range) {
+      range.deleteContents();
+      range.insertNode(this.root);
     }
   }]);
 
@@ -163,11 +186,13 @@ var Component = /*#__PURE__*/function () {
     _classCallCheck(this, Component);
 
     this.children = [];
+    this.props = Object.create(null);
   }
 
   _createClass(Component, [{
     key: "setAttribute",
     value: function setAttribute(name, value) {
+      this.props[name] = value;
       this[name] = value;
     }
   }, {
@@ -176,10 +201,46 @@ var Component = /*#__PURE__*/function () {
       this.children.push(child);
     }
   }, {
-    key: "mountTo",
-    value: function mountTo(parent) {
+    key: "update",
+    value: function update() {
+      var placeholder = document.createComment('placeholder');
+      var range = document.createRange();
+      range.setStart(this.range.endContainer, this.range.endOffset);
+      range.setEnd(this.range.endContainer, this.range.endOffset);
+      range.insertNode(placeholder);
+      this.range.deleteContents();
       var vdom = this.render();
-      vdom.mountTo(parent);
+      vdom.mountTo(this.range); // placeholder.parentNode.removeChild(placeholder);
+    }
+  }, {
+    key: "setState",
+    value: function setState(state) {
+      var merge = function merge(oldState, newState) {
+        for (var p in newState) {
+          if (_typeof(newState[p]) === 'object') {
+            if (_typeof(oldState[p]) !== 'object') {
+              oldState[p] = {};
+            }
+
+            merge(oldState[p], newState[p]);
+          } else {
+            oldState[p] = newState[p];
+          }
+        }
+      };
+
+      if (!this.state && state) {
+        this.state = {};
+      }
+
+      merge(this.state, state);
+      this.update();
+    }
+  }, {
+    key: "mountTo",
+    value: function mountTo(range) {
+      this.range = range;
+      this.update();
     }
   }]);
 
@@ -188,7 +249,6 @@ var Component = /*#__PURE__*/function () {
 var ToyReact = {
   createElement: function createElement(type, attributes) {
     var element;
-    debugger;
 
     if (typeof type === 'string') {
       element = new ElementWrapper(type);
@@ -237,7 +297,17 @@ var ToyReact = {
     return element;
   },
   render: function render(vdom, element) {
-    vdom.mountTo(element);
+    var range = document.createRange();
+
+    if (element.children.length) {
+      range.setStartAfter(element.lastChild);
+      range.setEndAfter(element.lastChild);
+    } else {
+      range.setStart(element, 0);
+      range.setEnd(element, 0);
+    }
+
+    vdom.mountTo(range);
   }
 };
 
@@ -277,30 +347,82 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 
 
 
-var MyComponent = /*#__PURE__*/function (_Component) {
-  _inherits(MyComponent, _Component);
+var Square = /*#__PURE__*/function (_Component) {
+  _inherits(Square, _Component);
 
-  var _super = _createSuper(MyComponent);
+  var _super = _createSuper(Square);
 
-  function MyComponent() {
-    _classCallCheck(this, MyComponent);
+  function Square(props) {
+    var _this;
 
-    return _super.apply(this, arguments);
+    _classCallCheck(this, Square);
+
+    _this = _super.call(this, props);
+    _this.state = {
+      value: null
+    };
+    return _this;
   }
 
-  _createClass(MyComponent, [{
+  _createClass(Square, [{
     key: "render",
     value: function render() {
-      return _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("div", null, "cool", this.children);
+      var _this2 = this;
+
+      return _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("button", {
+        className: "square",
+        onClick: function onClick() {
+          return _this2.setState({
+            value: 'X'
+          });
+        }
+      }, this.state.value || '');
     }
   }]);
 
-  return MyComponent;
+  return Square;
 }(_ToyReact__WEBPACK_IMPORTED_MODULE_0__["Component"]);
 
-var a = _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement(MyComponent, {
-  name: "a"
-}, _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("div", null, "123"));
+var Board = /*#__PURE__*/function (_Component2) {
+  _inherits(Board, _Component2);
+
+  var _super2 = _createSuper(Board);
+
+  function Board() {
+    _classCallCheck(this, Board);
+
+    return _super2.apply(this, arguments);
+  }
+
+  _createClass(Board, [{
+    key: "renderSquare",
+    value: function renderSquare(i) {
+      var _this3 = this;
+
+      return _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement(Square, {
+        value: i,
+        onClick: function onClick() {
+          return _this3.props.onClick(i);
+        }
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("div", null, _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("div", {
+        className: "board-row"
+      }, this.renderSquare(0), this.renderSquare(1), this.renderSquare(2)), _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("div", {
+        className: "board-row"
+      }, this.renderSquare(3), this.renderSquare(4), this.renderSquare(5)), _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("div", {
+        className: "board-row"
+      }, this.renderSquare(6), this.renderSquare(7), this.renderSquare(8)));
+    }
+  }]);
+
+  return Board;
+}(_ToyReact__WEBPACK_IMPORTED_MODULE_0__["Component"]);
+
+var a = _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement(Board, null);
 _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].render(a, document.body);
 
 /***/ })
